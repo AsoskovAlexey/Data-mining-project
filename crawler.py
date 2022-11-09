2872d669b303906b165b37d595d7cce4e0370729
Asoskov Alexey <xela1299@gmail.com>
2022-11-09 23:26:09 +0200

    Implemented writing the result into txt and endless  scraping. Actually, I found out, that there are about 785  distinct cards on the page. Datafile.txt contains the scraped result.

--- crawler.py
+++ crawler.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import json
 
 # Setting up driver
 SER = Service(r"C:\Program Files (x86)\chromedriver.exe")
@@ -12,6 +13,7 @@ driver = webdriver.Chrome(service=SER, options=OP)
 URL_TEMPLATE = ".html"
 
 result_dict = {}  # create a dictionary for results
+
 
 def path_to_element(elem_number: int):
     """returns a XPATH to element, works only on the special offer page"""
@@ -34,6 +36,7 @@ def get_id(url):
 
 
 def unpack_card_info(card_info, url):
+    """unpacks info from the card contents into a list"""
     card = card_info.splitlines()
     card.append(prettify_url(url))
     card[4] = card[4].strip(" Sold")
@@ -41,6 +44,7 @@ def unpack_card_info(card_info, url):
 
 
 def scroll():
+    """scrolls the page down"""
     last_height = driver.execute_script("return document.body.scrollHeight")
     # Scroll down to bottom
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -51,23 +55,41 @@ def scroll():
         last_height = new_height
 
 
-def scrape(url: str, maximum: int):
-    """main scraping engine"""
+def write_into_txt(file):
+    """write the output dictionary as txt file for further work"""
+    with open('datafile.txt', 'w') as data_file:
+        data_file.write(json.dumps(file))
+    print(f"{len(file)} entries saved into datafile!")
+
+
+def scrape(elements_scraped):
+    """waits for elements to appear, scrolls the page down and collects card info"""
+    WebDriverWait(driver, 10).until(  # wait until the element appears
+        EC.presence_of_element_located((By.XPATH, path_to_element(elements_scraped))))
+    scroll()
+    collect_card_info(elements_scraped)
+
+
+def main_sequence(url: str, maximum: int = 0):
+    """main scraping sequence """
     driver.get(url)
     elements_scraped = 10
     try:
-        while elements_scraped <= maximum:
-            WebDriverWait(driver, 10).until(  # wait until the element appears
-                EC.presence_of_element_located((By.XPATH, path_to_element(elements_scraped))))
-            collect_card_info(elements_scraped)
-            scroll()
-            elements_scraped += 10
+        if maximum != 0:  # runs a loop, that makes maximum-1 steps
+            while elements_scraped <= maximum:
+                scrape(elements_scraped)
+                elements_scraped += 10
+        else:
+            while True:  # if no value was passed, runs infinitely
+                scrape(elements_scraped)
+                elements_scraped += 10
     except Exception as err:
         print(f"Timeout! Page can not be parsed:{err}")
     driver.quit()
 
 
 def collect_card_info(end: int):
+    """collects the information from individual item cards"""
     for i in range(end - 10, end):
         if i != 0:
             element = driver.find_element(by=By.XPATH, value=path_to_element(i))
@@ -75,7 +97,6 @@ def collect_card_info(end: int):
             card_info = element.get_attribute('outerText')
             result_dict[get_id(url)] = unpack_card_info(card_info, url)
             print(f"collected {i} results")
-
 
 
 def all_tests():
@@ -86,6 +107,6 @@ def all_tests():
 
 
 if __name__ == '__main__':
-    scrape("https://campaign.aliexpress.com/wow/gcp/new-user-channel/index", 100)
-    print("the dictionary has", len(result_dict), "entries")
+    main_sequence("https://campaign.aliexpress.com/wow/gcp/new-user-channel/index", 2500)
     #all_tests()
+    write_into_txt(result_dict)
