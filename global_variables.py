d34b0ee9dd465c298583d26712ec628b6ed66f36
Denis Laevskiy <lade.denis@gmail.com>
2022-11-30 20:38:50 +0200

    Added 404 page for faster scraper initiation. Added a loop for pages (TODO: split into functions), now it is possible to scrape the whole category.

--- global_variables.py
+++ global_variables.py
@@ -4,5 +4,5 @@ class Constants:
     DB_CONFIG_FILE = "db/db_config.json"
     DB_STRUCTURE_FILE = "db/project_db_structure.sql"
     DB_NAME = "aliexpress"
-    ROOT_URL = "https://www.aliexpress.com/"
+    URL_404 = "https://www.aliexpress.com/p/error/404.html"
     COOKIES_FILE = "cookies.json"
--- main.py
+++ main.py
@@ -26,12 +26,12 @@ def main():
     print_log(f"Main function started: {time.asctime(time.localtime(main_start_time))}")
 
     # NOTE: url and categories are hardcoded, but will be replaced in the future.
-    url = "https://www.aliexpress.com/category/708042/cpus.html"
+    category_url = "https://www.aliexpress.com/category/708042/cpus.html"
     category = 0
 
     # Database initialization
     db = MySQL(read_json(Constants.DB_CONFIG_FILE))
-    db_creation_script.start(mode="force")
+    db_creation_script.start(mode="skip")
     db.push(f"USE {Constants.DB_NAME};")
 
     # Scraper initialization
@@ -40,32 +40,47 @@ def main():
         f"Scraper initialization started: {time.asctime(time.localtime(scraper_initialization_start_time))}"
     )
     scraper = Scraper(silent_mode=True, scroll_pause_time=1)
-    scraper.get_page(Constants.ROOT_URL)
+    scraper.get_page(Constants.URL_404)
     scraper.add_cookies(read_json(Constants.COOKIES_FILE))
     print_log(
         f"Scraper initialization ended in: {round(time.time() - scraper_initialization_start_time, 2)} seconds"
     )
 
-    page = Page(scraper.get_page(url))
+    page_with_products = Page(scraper.get_page(category_url))
+    n_pages = page_with_products.get_n_pages()
 
-    item = 0
-    start_time = time.time()
-    print_log(f"Scraping from page started: {time.asctime(time.localtime(start_time))}")
+    category_start_time = time.time()
+    print_log(
+        f"Scraping from category started: {time.asctime(time.localtime(category_start_time))}"
+    )
 
-    for url in page.get_links():
-        try:
-            product = Product(url, scraper.get_page(url))
-            db.push(
-                f'REPLACE INTO products (id, link, title, rating, n_reviews, n_orders, price, category_id)\
-                VALUES ({product.id}, "{product.link}", "{product.title}", {product.rating}, {product.n_reviews}, {product.n_orders}, {product.price}, {category});'
-            )
-            item += 1
-            print(f"Product #{item} - id: {product.id}")
-        except Exception as e:
-            print(f"Unable to process product. URL: {url}\nError: {e}")
+    product_number = 0
+    for page in range(1, n_pages + 1):
+        current_url = category_url + f"?&page={page}"
+        page_with_products = Page(scraper.get_page(current_url))
+        print(current_url)
+        page_start_time = time.time()
+        print_log(
+            f"Scraping from page started: {time.asctime(time.localtime(page_start_time))}"
+        )
+        for url in page_with_products.get_links():
+            try:
+                product = Product(url, scraper.get_page(url))
+                db.push(
+                    f'REPLACE INTO products (id, link, title, rating, n_reviews, n_orders, price, category_id)\
+                    VALUES ({product.id}, "{product.link}", "{product.title}", {product.rating}, {product.n_reviews}, {product.n_orders}, {product.price}, {category});'
+                )
+                product_number += 1
+                print(f"Product #{product_number} - id: {product.id}")
+            except Exception as e:
+                print(f"Unable to process product. URL: {url}\nError: {e}")
+
+        print_log(
+            f"Scraping from page ended in: {round(time.time() - page_start_time, 2)} seconds"
+        )
 
     print_log(
-        f"Scraping from page ended in: {round(time.time() - start_time, 2)} seconds"
+        f"Scraping from category ended in: {round(time.time() - category_start_time, 2)} seconds"
     )
 
     print_log(
