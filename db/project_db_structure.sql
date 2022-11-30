e89ed6a29ac5366def676fdc4ef3ebb3298d7a47
Denis Laevskiy <lade.denis@gmail.com>
2022-11-30 13:14:15 +0200

    SQL keywords now capitalized in db/project_db_structure.sql

--- db/project_db_structure.sql
+++ db/project_db_structure.sql
@@ -1,41 +1,41 @@
 CREATE TABLE products (
   id BIGINT NOT NULL,
-  link varchar(1024) NOT NULL,
-  title varchar(1024),
-  rating float,
-  n_reviews int,
-  n_orders int,
-  price float,
-  category_id int NOT NULL,
-  PRIMARY KEY (ID)
+  link VARCHAR(1024) NOT NULL,
+  title VARCHAR(1024),
+  rating FLOAT,
+  n_reviews INT,
+  n_orders INT,
+  price FLOAT,
+  category_id INT NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE category_level_3 (
-  id int NOT NULL,
-  title varchar(255),
-  parent_id int NOT NULL,
-  PRIMARY KEY (ID)
+  id INT NOT NULL,
+  title VARCHAR(255),
+  parent_id INT NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE category_level_2 (
-  id int NOT NULL,
-  title varchar(255),
-  parent_id int NOT NULL,
-  PRIMARY KEY (ID)
+  id INT NOT NULL,
+  title VARCHAR(255),
+  parent_id INT NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE category_level_1 (
-  id int NOT NULL,
-  title varchar(255),
-  PRIMARY KEY (ID)
+  id INT NOT NULL,
+  title VARCHAR(255),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE reviews (
-  id int NOT NULL,
-  product_id int NOT NULL,
-  product_type varchar(1024),
+  id INT NOT NULL,
+  product_id INT NOT NULL,
+  product_type VARCHAR(1024),
   comment TEXT,
-  PRIMARY KEY (ID)
+  PRIMARY KEY (id)
 );
 
 #ALTER TABLE products ADD FOREIGN KEY(category_id) REFERENCES category_level_3(id);
