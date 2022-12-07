5f50cd323e1f143235057bbf15c6b2523a2e5090
Denis Laevskiy <lade.denis@gmail.com>
2022-12-07 22:54:27 +0200

    Created logger

--- global_variables.py
+++
@@ -1,9 +0,0 @@
-class Constants:
-    """Constants"""
-
-    DB_CONFIG_FILE = "db/db_config.json"
-    DB_STRUCTURE_FILE = "db/project_db_structure.sql"
-    DB_NAME = "aliexpress"
-    URL_404 = "https://www.aliexpress.com/p/error/404.html"
-    COOKIES_FILE = "cookies.json"
-    ERROR_LOG_FILE = "error_log.txt"
--- log/logger.py
+++ log/logger.py
@@ -2,22 +2,42 @@ from global_functions import read_configuration, append_file
 
 
 class Logger:
+    logger_id = 0
+
+    __slots__ = ["__error_log_file", "__log_file", "__id", "__name"]
+
     def __init__(self):
         """Initialize the logger"""
-        configuration = read_configuration()["log"]
-        self.__error_log_file = configuration["error_log_file"]
-        self.__log_file = configuration["log_file"]
 
-    def write_log(self, text):
+        def get_personal_path(path):
+            """Returns the path specified by logger id"""
+            path = path.split("/")
+            path[-1] = f"{self.__name}_{path[-1]}"
+            return "/".join(path)
+
+        self.__id = Logger.logger_id
+        self.__name = f"logger{self.__id}"
+        Logger.logger_id += 1
+
+        configuration = read_configuration()["log"]
+
+        self.__error_log_file = get_personal_path(configuration["error_log_file"])
+        self.__log_file = get_personal_path(configuration["log_file"])
+
+    def write_log(self, text, display=True):
         """Write the log to the log file specified in the configuration"""
+        if display:
+            print(text)
         append_file(self.__log_file, text)
 
-    def write_error_log(self, text):
+    def write_error_log(self, text, display=True):
+        if display:
+            print(text)
         """Write the log to the log file specified in the configuration"""
         append_file(self.__error_log_file, text)
 
 
 def add_brackets(text, style="="):
-    """Prints the log in an easy-to-read format"""
+    """Returns the text in the brackets"""
     brackets = len(text) * style
     return f"{brackets}\n{text}\n{brackets}\n"
